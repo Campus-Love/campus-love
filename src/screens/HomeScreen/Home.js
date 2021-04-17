46e3423529d0a0cff5ecb7ef10fe2bd6e0bc89a0
katznicho <katznicho@gmail.com>
2021-04-17 12:50:17 +0300

    worked on appbar and home screen

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -1,84 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { EvilIcons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import {theme} from "../../theme"
+import React from "react"
+import {View, Text, SafeAreaView, Image, StyleSheet} from "react-native"
+import CampuserData from "../../components/FlatList";
 
 
-//create tabs
-const Tab  = createBottomTabNavigator()
-
-//Default tab
-const DefaultTab = ()=>{
-    return (<View><Text>Hello </Text></View>)
-}
-const HomeScreen = () => {
+const Home = ()=>{
     return (
-        <Tab.Navigator
-      initialRouteName="Home"
-      tabBarOptions={{
-        activeTintColor: `${theme.colors.primary}`,
-        inactiveTintColor:`${theme.colors.placeholder}`,
-        tabStyle:{
-            marginBottom:5,
-            PaddingLeft:2,
-            paddingRight:2,
-            marginTop:5
-        }
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={DefaultTab}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={24} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={DefaultTab}
-        options={{
-          tabBarLabel: 'Search',
-          tabBarIcon: ({ color, size }) => (
-            <EvilIcons name="search" size={30} color={color} />
-          ),
-        
-        }}
-      />
-      <Tab.Screen
-        name="Camera"
-        component={DefaultTab}
-        options={{
-          tabBarLabel: 'Camera',
-          tabBarIcon: ({ color, size }) => (
-            <Entypo name="camera" size={24} color={color} />
-          ),
-        
-        }}
-      />
+        <View style = {styles.homeContainer}>
+                    <Image
+                    style = {StyleSheet.absoluteFillObject}
+            source  = {{uri:"https://media.gettyimages.com/photos/hes-one-of-the-popular-guys-picture-id500721035?s=612x612"}}
+            />
+           <CampuserData/>
+        </View>
 
-<Tab.Screen
-        name="Settings"
-        component={DefaultTab}
-        options={{
-          tabBarLabel: 'Settings',
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="setting" size={24} color={color} />
-          ),
-        
-        }}
-      />
-      
-    </Tab.Navigator>
     )
 }
 
-export default HomeScreen
-
-const styles = StyleSheet.create({})
+ const styles = StyleSheet.create({
+     homeContainer:{flex:1,backgroundColor:"#fff"}
+ })
+export default Home
