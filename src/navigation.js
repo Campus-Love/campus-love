1cf01845933c55c3512c2b44fb4fbc135d701968
katznicho <katznicho@gmail.com>
2021-04-24 10:36:23 +0300

    added camera functionality

--- src/navigation.js
+++ src/navigation.js
@@ -6,41 +6,47 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './screens/HomeScreen';
 import AppBar from './components/AppBar';
 import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
+import Photos from './screens/Photos/photos';
 
 
 const Stack = createStackNavigator();
 
-const Checker = ()=>{
+const Checker = () => {
   return (
     <View><Text>Checked</Text></View>
   )
 }
 
 
-const Navigation = ()=>{
-    return (
-        <NavigationContainer>
-        <Stack.Navigator
-        initialRouteName = "Home"
-        headerMode ="screen"       
-        >
-           <Stack.Screen name="Home" 
-           component = {LandingScreen}
-           options = {{
-             title:"Account",
-             header:(props)=>(<AppBar {...props} title = "CampusLove" showBadge/>),
-           }}
-           />
-           <Stack.Screen name="Profile"
-           options = {{
-             header:(props)=>(<AppBar {...props} title="Profile"/>)
-           }}
-            component ={ProfileScreen}/>
-        </Stack.Navigator>
-    
-      </NavigationContainer>
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        headerMode="screen"
+      >
+        <Stack.Screen name="Home"
+          component={LandingScreen}
+          options={{
+            title: "Account",
+            header: (props) => (<AppBar {...props} title="CampusLove" showBadge />),
+          }}
+        />
+        <Stack.Screen name="Profile"
+          options={{
+            header: (props) => (<AppBar {...props} title="Profile" />)
+          }}
+          component={ProfileScreen} />
+        <Stack.Screen name="Photos"
+          options={{
+            header: (props) => (<AppBar {...props} title="Picture" />)
+          }}
+          component={Photos} />
+      </Stack.Navigator>
 
-    )
+    </NavigationContainer>
+
+  )
 }
 
 export default Navigation;
